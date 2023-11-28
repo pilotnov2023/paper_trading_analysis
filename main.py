d486c6b4f472e8fc72547f2f6be28d39dcb98619
pilotnov2023 <PROJECT.PILOT.NOV2023@hotmail.com>
2023-11-28 12:28:24 +0330

    tabular print

--- main.py
+++ main.py
@@ -10,19 +10,22 @@ file_path = 'positions.csv'
 df = pd.read_csv(file_path)
 
 
-
 def calculate_long_close_price(position_open_price, long_profit):
     # Calculate close_price for a long position given long_profit and subtract exchange fee
-    close_price = position_open_price * (1 + (long_profit + EXCHANGE_FEE_PERCENT) / 100)
+    close_price = position_open_price * \
+        (1 + (long_profit + EXCHANGE_FEE_PERCENT) / 100)
     return close_price
 
 
 def calculate_short_close_price(position_open_price, short_profit):
     # Calculate close_price for a short position given short_profit and subtract exchange fee
-    close_price = position_open_price * (1 - (short_profit + EXCHANGE_FEE_PERCENT) / 100)
+    close_price = position_open_price * \
+        (1 - (short_profit + EXCHANGE_FEE_PERCENT) / 100)
     return close_price
 
 # Function to calculate lowest and highest prices based on runup and drawdown
+
+
 def calculate_lowest_highest(position):
     position_open_price = float(position["position_open_price"])
     direction = position["direction"]
@@ -30,16 +33,22 @@ def calculate_lowest_highest(position):
     current_position_drawdown = position["drawdown"]
 
     if direction == "long":
-        lowest_price = calculate_long_close_price(position_open_price , current_position_drawdown)
-        highest_price = calculate_long_close_price(position_open_price , current_position_runup)
+        lowest_price = calculate_long_close_price(
+            position_open_price, current_position_drawdown)
+        highest_price = calculate_long_close_price(
+            position_open_price, current_position_runup)
     else:  # Assuming short position
-        lowest_price = calculate_short_close_price(position_open_price , current_position_runup)
-        highest_price = calculate_short_close_price(position_open_price , current_position_drawdown)
+        lowest_price = calculate_short_close_price(
+            position_open_price, current_position_runup)
+        highest_price = calculate_short_close_price(
+            position_open_price, current_position_drawdown)
 
     return lowest_price, highest_price
 
+
 # Apply the function to each row of the DataFrame and assign the results to new columns
-df[['lowest_price', 'highest_price']] = df.apply(calculate_lowest_highest, axis=1, result_type='expand')
+df[['lowest_price', 'highest_price']] = df.apply(
+    calculate_lowest_highest, axis=1, result_type='expand')
 
 # Print the first 10 rows of the DataFrame
 # print(df.head(10))
@@ -87,23 +96,29 @@ print(f"8) Total Losses: {total_losses:.2f}")
 # 9) Total profit and loss by long, short transaction type
 total_profit_long = df[df['direction'] == 'long']['net_profit'].sum()
 total_profit_short = df[df['direction'] == 'short']['net_profit'].sum()
-print(f"9) Total Profit and Loss by Long Transactions: {total_profit_long:.2f}")
-print(f"   Total Profit and Loss by Short Transactions: {total_profit_short:.2f}")
+print(
+    f"9) Total Profit and Loss by Long Transactions: {total_profit_long:.2f}")
+print(
+    f"   Total Profit and Loss by Short Transactions: {total_profit_short:.2f}")
 
 # 10) Profit for long
-profit_for_long = df[(df['direction'] == 'long') & (df['net_profit'] > 0)]['net_profit'].sum()
+profit_for_long = df[(df['direction'] == 'long') & (
+    df['net_profit'] > 0)]['net_profit'].sum()
 print(f"10) Profit for Long Positions: {profit_for_long:.2f}")
 
 # 11) Loss for long
-loss_for_long = df[(df['direction'] == 'long') & (df['net_profit'] < 0)]['net_profit'].sum()
+loss_for_long = df[(df['direction'] == 'long') & (
+    df['net_profit'] < 0)]['net_profit'].sum()
 print(f"11) Loss for Long Positions: {loss_for_long:.2f}")
 
 # 12) Profit for short
-profit_for_short = df[(df['direction'] == 'short') & (df['net_profit'] > 0)]['net_profit'].sum()
+profit_for_short = df[(df['direction'] == 'short') & (
+    df['net_profit'] > 0)]['net_profit'].sum()
 print(f"12) Profit for Short Positions: {profit_for_short:.2f}")
 
 # 13) Loss for short
-loss_for_short = df[(df['direction'] == 'short') & (df['net_profit'] < 0)]['net_profit'].sum()
+loss_for_short = df[(df['direction'] == 'short') & (
+    df['net_profit'] < 0)]['net_profit'].sum()
 print(f"11) Loss for Short Positions: {loss_for_short:.2f}")
 
 
@@ -124,21 +139,21 @@ print(f"12) Overall Risk/Reward Ratio: {overall_risk_reward_ratio:.2f}")
 
 # Display results in tabular format
 results_table = [
-    ["0) Number Of Trades", total_positions],
-    ["1) Trading Success Rate", f"{success_rate:.2%}"],
-    ["2) Total Profit and Loss", f"{total_profit_loss:.2f}"],
-    ["3) Average Profit", f"{average_profit:.2f}"],
-    ["4) Average Loss", f"{average_loss:.2f}"],
-    ["5) Biggest Profit", f"{biggest_profit:.2f}"],
-    ["6) Biggest Loss", f"{biggest_loss:.2f}"],
-    ["7) Total Profits", f"{total_profits:.2f}"],
-    ["8) Total Losses", f"{total_losses:.2f}"],
-    ["9) Total Profit and Loss by Long Transactions", f"{total_profit_long:.2f}", f"{total_profit_short:.2f}"],
-    ["10) Profit for Long Positions", f"{profit_for_long:.2f}"],
-    ["11) Loss for Long Positions", f"{loss_for_long:.2f}"],
-    ["12) Profit for Short Positions", f"{profit_for_short:.2f}"],
-    ["13) Loss for Short Positions", f"{loss_for_short:.2f}"],
-    ["14) Overall Risk/Reward Ratio", f"{overall_risk_reward_ratio:.2f}"]
+    ["0", "Number Of Trades", total_positions],
+    ["1", "Trading Success Rate", f"{success_rate:.2%}"],
+    ["2", "Total Profit and Loss", f"{total_profit_loss:.2f}"],
+    ["3", "Average Profit", f"{average_profit:.2f}"],
+    ["4", "Average Loss", f"{average_loss:.2f}"],
+    ["5", "Biggest Profit", f"{biggest_profit:.2f}"],
+    ["6", "Biggest Loss", f"{biggest_loss:.2f}"],
+    ["7", "Total Profits", f"{total_profits:.2f}"],
+    ["8", "Total Losses", f"{total_losses:.2f}"],
+    ["9", "Total Profit and Loss by Long Transactions",f"{total_profit_long:.2f}", f"{total_profit_short:.2f}"],
+    ["10", "Profit for Long Positions", f"{profit_for_long:.2f}"],
+    ["11", "Loss for Long Positions", f"{loss_for_long:.2f}"],
+    ["12", "Profit for Short Positions", f"{profit_for_short:.2f}"],
+    ["13", "Loss for Short Positions", f"{loss_for_short:.2f}"],
+    ["14", "Overall Risk/Reward Ratio", f"{overall_risk_reward_ratio:.2f}"]
 ]
 
-print(tabulate(results_table, headers=["Metric", "Value"], tablefmt="fancy_grid"))
+print(tabulate(results_table, headers=["Row","Metric", "Value"], tablefmt="pretty", colalign=("center", "center")))
